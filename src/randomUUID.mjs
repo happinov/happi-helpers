4504fb34f59d3d72f421a9a2888f43ab7ed3068c
Nicolas Leclerc <nicolas@coqsenpate.com>
2024-12-16 17:42:20 +0100

    Fixed randomUUID error.

--- src/randomUUID.mjs
+++ src/randomUUID.mjs
@@ -1,17 +1,14 @@
 // Copied from https://github.com/ungap/random-uuid
 
-if (crypto.randomUUID) {
-	console.debug('crypto.randomUUID polyfill not required.')
-	return
-}
+if (!crypto.randomUUID) {
+	console.debug('Enabling crypto.randomUUID polyfill.')
 
-console.debug('Enabling crypto.randomUUID polyfill.')
-
-// https://stackoverflow.com/a/2117523/2800218
-// LICENSE: https://creativecommons.org/licenses/by-sa/4.0/legalcode
-crypto.randomUUID = function randomUUID() {
-	return (
-		[1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,
-			c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-	)
+	// https://stackoverflow.com/a/2117523/2800218
+	// LICENSE: https://creativecommons.org/licenses/by-sa/4.0/legalcode
+	crypto.randomUUID = function randomUUID() {
+		return (
+			[1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,
+				c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+		)
+	}
 }
