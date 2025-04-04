bbfd817ba243b75f374c2976781762369f17bac1
Nicolas Leclerc <nicolas@coqsenpate.com>
2025-04-04 16:06:42 +0200

    Added setTimeout and setInterval methods to marionette views.

--- src/marionette.mjs
+++ src/marionette.mjs
@@ -18,8 +18,24 @@ export default (Marionette)=> {
 		})
 	}
 
+	function setIntervalAutorelease(callback, value) {
+		const id = setInterval(callback.bind(this),value)
+		this.on('before:detach', ()=> clearInterval(id))
+		return id
+	}
+
+	function setTimeoutAutorelease(callback, value) {
+		const id = setTimeout(callback.bind(this),value)
+		this.on('before:detach', ()=> clearTimeout(id))
+		return id
+	}
+
 	Marionette.View.prototype.ensureRendered = ensureRendered
 	Marionette.View.prototype.ensureAttached = ensureAttached
+	Marionette.View.prototype.setInterval = setIntervalAutorelease
+	Marionette.View.prototype.setTimeout = setTimeoutAutorelease
 	Marionette.CollectionView.prototype.ensureRendered = ensureRendered
 	Marionette.CollectionView.prototype.ensureAttached = ensureAttached
+	Marionette.CollectionView.prototype.setInterval = setIntervalAutorelease
+	Marionette.CollectionView.prototype.setTimeout = setTimeoutAutorelease
 }
